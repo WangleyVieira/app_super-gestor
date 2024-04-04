c221a2f6054d23a117721d447d372de187115298
WangleyVieira <wangleymsv10@hotmail.com>
2024-04-03 22:55:48 -0400

    atualizacoes switch e for

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,7 +10,30 @@ class FornecedorController extends Controller
     public function index ()
     {
         try{
-            return view('app.fornecedor.index');
+            $fornecedores = [
+                0 => [
+                    'nome' => 'Fornecedor 1',
+                    'status' => 'N',
+                    'cnpj' => '00.000.000/000-00',
+                    'ddd' => '11', //SP
+                    'telefone' => '0000-0000'
+                ],
+                1 => [
+                    'nome' => 'Fornecedor 2',
+                    'status' => 'S',
+                    'ddd' => '85', //CE
+                    'telefone' => '0000-0000'
+                ],
+                2 => [
+                    'nome' => 'Fornecedor 2',
+                    'status' => 'S',
+                    'ddd' => '32', //MG
+                    'telefone' => '0000-0000'
+                ]
+            ];
+
+            return view('app.fornecedor.index', compact('fornecedores'));
+
         }
         catch(Exception $ex){
             return $ex->getMessage();
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,48 @@
 <h3>
     Fornecedor
 </h3>
+
+@php
+    /*
+    if(isset($variavel)) {} //retornar true se a variável estiver definida
+    */
+@endphp
+
+{{-- @isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+    <br>
+    Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
+    @switch( $fornecedores[0]['ddd'])
+        @case('11')
+                São Paulo - SP
+            @break
+
+        @case('32')
+                Juiz de Fora - MG
+            @break
+
+        @case('85')
+                Fortaleza - CE
+            @break
+
+        @default
+            Estado não identificado
+
+    @endswitch
+@endisset --}}
+@isset($fornecedores)
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        Telefone: {{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <hr>
+    @endfor
+@endisset
